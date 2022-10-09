4eedb1a4aa50d63db879590289a8f5ffc19af568
eccnil <eccnil@gmail.com>
2022-10-09 02:11:52 +0200

    modulos serapados

--- restaurant/src/lib.rs
+++ restaurant/src/lib.rs
@@ -1,14 +1,8 @@
-mod front_of_house {
-    pub mod hosting {
-        pub fn add_to_waitlist() {}
-        pub fn seat_at_table() {}
-    }
-    mod serving {
-        fn take_order() {}
-        fn serve_order() {}
-        fn take_payment() {}
-    }
-}
+use std::{cmp::Ordering, io, self};
+
+mod front_of_house;
+
+pub use crate::front_of_house::hosting;
 
 mod back_of_house {
     pub struct Breakfast {
