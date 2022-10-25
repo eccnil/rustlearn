16c9fdb93473c010188c21ea7bbdd0662fe33a1e
eccnil <eccnil@gmail.com>
2022-10-26 01:02:57 +0200

    threads and messages

--- my-project/src/bin/concurrency.rs
+++ my-project/src/bin/concurrency.rs
@@ -1,7 +1,7 @@
 fn main (){
     println!("lets start with threads");
     threads();
-    //message
+    messages();
     //shared state
     //sinc & send traits
 }
@@ -26,4 +26,64 @@ fn threads() {
     }
 
     handle.join().unwrap();
+}
+
+fn messages(){
+    use std::sync::mpsc; // multiple producers sigle consumer
+    use std::thread;
+    use std::time::Duration;
+
+    let (tx, rx) = mpsc::channel();
+
+    let _sender = thread::spawn(move || {
+        let val = String::from("hi");
+        //let val:i32 = 3; //para un entero (pila) mantenemos su ownership, no como con el string
+        match tx.send(val) {
+            Ok(()) => (),
+            Err(e) => println!("error sending via channel: {}", e),
+        };
+        //println!("ya no tengo val, esta linea o compila {}", val);
+    });
+
+    let receiver = thread::spawn(move || {
+        let received = rx.recv().unwrap(); // try_recv no bloquea el hilo
+        println!("received via channel: {received}");
+    });
+
+    receiver.join().unwrap();
+
+    // muliple producers
+    let (tx, rx) = mpsc::channel();
+    let tx1 = tx.clone();
+
+    let _sender1 = thread::spawn( move || {
+        let vals = vec![
+            String::from("hi"),
+            String::from("from"),
+            String::from("the"),
+            String::from("thread"),
+        ];
+        for val in vals {
+            tx1.send(val).unwrap();
+            thread::sleep(Duration::from_millis(1));
+        }
+    });
+
+    let _sender2 = thread::spawn( move || {
+        let vals = vec![
+            String::from("more"),
+            String::from("messages"),
+            String::from("for"),
+            String::from("you"),
+        ];
+        for val in vals {
+            tx.send(val).unwrap();
+            thread::sleep(Duration::from_millis(1));
+        }
+    });
+
+    for received in rx {
+        println!("Got: {received} from a channel of two");
+    }
+
 }
